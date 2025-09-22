1fb8e8137623105b49755a048b8d61e16d88c35a
Mohamed Khaldi <22302683@me.uvsq.fr>
2025-09-22 15:20:12 +0200

    2 Mobiles faisant aller-retour sur 2 lignes et 1 colonne

--- UneFenetre.java
+++ UneFenetre.java
@@ -11,13 +11,24 @@ class UneFenetre extends JFrame
         setSize(LARG,HAUT);
         setVisible(true);
         Container container = getContentPane();
-        UnMobile sonMobile = new UnMobile(LARG, HAUT);
+        container.setLayout (new GridLayout(2 , 1)) ;
+        
+        UnMobile sonMobile = new UnMobile(LARG/2, HAUT/2);
+        UnMobile sonMobile2 = new UnMobile(LARG/2, HAUT/2);
+        
         container.add(sonMobile);
-        Thread thread = new Thread(sonMobile);
-        thread.start();
+        container.add(sonMobile2);
+    
+        
+        Thread thread1 = new Thread(sonMobile);
+        Thread thread2 = new Thread(sonMobile2);
+    
+
+        thread1.start(); 
+        thread2.start();
 	// TODO 
 	// ajouter sonMobile a la fenetre
-	// creer une thread laThread avec sonMobile
+	// creer une thread1 laThread avec sonMobile
 	// afficher la fenetre
 	// lancer laThread 
     }
